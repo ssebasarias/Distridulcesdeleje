df835dfeb453f78a3a7de001abdd3563cca22052
FelipeeMartinez2 <martifelipe219@gmail.com>
2024-02-22 13:12:42 -0500

    proyecto traslado a php

---
+++ controladores/controlador_guardar_imagen.php
@@ -0,0 +1,17 @@
+<?php
+
+include("../configuraciones/bd.php");
+
+$nombre =  $_POST['nombre'];
+$imagen = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
+
+$query = "INSERT INTO imgsbanner(nombre,imagen) VALUES ('$nombre','$imagen')";
+$resultado = $conexion->query($query);
+
+if ($resultado) {
+    header("Location: ../indexcrud.php");
+} else {
+    echo "No se inserto recague la pagina"; 
+}
+
+?>
